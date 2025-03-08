e724e1e9b220b152de612572b03bde5f6c2b4b47
SK <limsweekiat@gmail.com>
2025-03-08 23:33:46 +0800

    Add read_file tool

--- file_ops.py
+++ file_ops.py
@@ -6,3 +6,14 @@ def create_file(filepath: str) -> None:
     """
     with open(filepath, "w"):
         pass
+
+
+def read_file(filepath: str) -> str:
+    """
+    Create a file at filepath
+    Args:
+    - filepath (str): Filepath of file to write (required)
+    """
+    with open(filepath, "r") as file:
+        contents = file.read()
+    return contents
