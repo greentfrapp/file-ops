54124a6a346629d58b597102a5ed18489429c7ae
SK <limsweekiat@gmail.com>
2025-03-08 23:36:30 +0800

    Fix read_file docstring and add tool to manifest

--- file_ops.py
+++ file_ops.py
@@ -12,7 +12,7 @@ def read_file(filepath: str) -> str:
     """
     Create a file at filepath
     Args:
-    - filepath (str): Filepath of file to write (required)
+    - filepath (str): Filepath of file to read (required)
     """
     with open(filepath, "r") as file:
         contents = file.read()
